04a306c6324da874c192acbd414b7069b34b4dfd
Aleksey <krut.slava@inbox.ru>
2021-06-29 11:08:19 +0300

    first commit

--- app/src/main/java/com/example/pr118gryzlov/MainActivity.java
+++ app/src/main/java/com/example/pr118gryzlov/MainActivity.java
@@ -3,16 +3,44 @@ package com.example.pr118gryzlov;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton firstBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       ;
+
+
+
+    }
+    public void checkClick(View v){
+        String result = "";
+        switch (v.getId()){
+            case R.id.imageButton3:
+                result = "1";
+                break;
+            case R.id.imageButton4:
+                result = "2";
+                break;
+            case R.id.imageButton5:
+                result = "3";
+                break;
+            case R.id.imageButton7:
+                result = "4";
+                break;
+            case R.id.imageButton9:
+                result = "5";
+                break;
+        }
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 
 }
